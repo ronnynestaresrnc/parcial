66c821c3b83f8fe494a43fdd55decbdc3633c8c4
ronnynestaresrnc <rnestares29@gmail.com>
2021-06-03 17:34:20 -0500

    master

---
+++ aboutFetch.js
@@ -0,0 +1,14 @@
+
+
+export default 
+  function fetchAbout ()  {
+    fetch("About.html")
+      .then(res => res.text())
+      .then(about => {
+
+        $("#content-table").html(about);
+      })
+  
+    
+
+    }
